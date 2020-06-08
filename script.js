b08df1125559f0bbf2f9c6a05a9fa883acd60df4
imac <chadmdubay@gmail.com>
2020-06-08 11:55:16 -0400

    Updated script.js with completed password generator code.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var x = document.querySelector("#password");
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var capitalAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
@@ -42,14 +43,15 @@ function writePassword()
 
   for(i = 0; i < numberOfCharactersConfirm; i++)
   {
-      var characterSelection = Math.floor(Math.random() * characterArray.length);
-      var characterSelection2 =  Math.floor(Math.random() * characterArray[characterSelection].length);
-      emptyPasswordArray.push(characterArray[characterSelection[characterSelection2]]);
+      var characterSelection = characterArray[Math.floor(Math.random() * characterArray.length)]
+      var characterSelection2 =  characterSelection[Math.floor(Math.random() * characterArray.length)]
+      emptyPasswordArray.push(characterSelection2);
   }
 
-  var newPassword = emptyPasswordArray.join();
+  var newPassword = emptyPasswordArray.join("");
 
-  console.log(characterArray);
+  console.log(newPassword);
+  x.setAttribute('placeholder',newPassword);
 
 }
 
